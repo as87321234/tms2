f4ea7017c29de2386153f7ff35bcbf6a449495ed
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-18 13:27:18 -0400

    Remove dead code

--- src/main/java/ca/mgisinc/tms2/model/User.java
+++ src/main/java/ca/mgisinc/tms2/model/User.java
@@ -30,17 +30,4 @@ public class User extends BaseEntity {
 			inverseJoinColumns = @JoinColumn(
 					name = "role_id", referencedColumnName = "id"))
 	private Collection<Role> roles;
-	
-	public User(String username, String firstName, String lastName, String email, String password, Boolean enabled, Boolean tokenExpired) {
-		
-		this.username = username;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-		this.enabled = enabled;
-		this.tokenExpired = tokenExpired;
-		
-	}
-	
 }
