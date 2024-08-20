6a7031cedf249ed81f30711c37f9cd2f1bcc2565
8937753 CANADA INC - Andre St-Louis <as87321234@gmail.com>
2024-08-20 18:42:14 -0400

    Add FlowableApiControllerConfig

--- src/main/java/ca/mgisinc/tms2/controller/ApplicationApiController.java
+++ src/main/java/ca/mgisinc/tms2/controller/ApplicationApiController.java
@@ -1,5 +1,6 @@
 package ca.mgisinc.tms2.controller;
 
+import ca.mgisinc.tms2.form.BiometricCollectionForm;
 import ca.mgisinc.tms2.model.Privilege;
 import ca.mgisinc.tms2.model.Role;
 import ca.mgisinc.tms2.model.User;
@@ -13,12 +14,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -30,16 +36,22 @@ import java.util.Set;
 public class ApplicationApiController {
 	
 	private final Logger log = LoggerFactory.getLogger(ApplicationApiController.class);
-	UserRepository userRepository;
+	private final RestTemplate restTemplate = new RestTemplate();
+	
+	private UserRepository userRepository;
+	
+	private FlowableApiControllerConfig conf;
 	
 	@Autowired
-	public ApplicationApiController(UserRepository userRepository) {
+	public ApplicationApiController(FlowableApiControllerConfig conf, UserRepository userRepository) {
+		this.conf = conf;
 		this.userRepository = userRepository;
 	}
+	
 	/**
 	 * Returns an list of all users from the database.
 	 */
-	@PostMapping(value = "get_users", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value = "/get_users", produces = MediaType.APPLICATION_JSON_VALUE)
 	public DataView getUser() {
 		
 		log.info("ApplicationController root");
